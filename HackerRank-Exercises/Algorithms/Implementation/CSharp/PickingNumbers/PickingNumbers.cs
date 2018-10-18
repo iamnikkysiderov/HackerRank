5060d49a617d2b1be9017a29297b8194cbbfebc0
Nikola Siderov <iamnikkysiderov@gmail.com>
2018-10-18 16:37:18 +0300

    Added all of my problems and exercises I have solved in hackerrank.com

---
+++ HackerRank-Exercises/Algorithms/Implementation/CSharp/PickingNumbers/PickingNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickingNumbers // unsolved - 4/10
+{
+    class PickingNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> arr = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int counter = 0;
+            List<int> counters = new List<int>();
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = 0; j < arr.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (Math.Abs(arr[i] - arr[j]) == 0 || Math.Abs(arr[i] - arr[j]) == 1)
+                    {
+                        counter++;
+                    }
+                }
+
+                counters.Add(counter);
+
+                counter = 0;
+            }
+
+            Console.WriteLine(counters.Max());
+        }
+    }
+}
