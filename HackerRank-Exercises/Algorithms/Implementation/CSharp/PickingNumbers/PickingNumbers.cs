7c6178c724c6c9af0d954047b8c59f74e22b432e
Nikola Siderov <iamnikkysiderov@gmail.com>
2018-12-04 19:20:44 +0300

    edited an exercise from implementation, algorithms

--- HackerRank-Exercises/Algorithms/Implementation/CSharp/PickingNumbers/PickingNumbers.cs
+++ HackerRank-Exercises/Algorithms/Implementation/CSharp/PickingNumbers/PickingNumbers.cs
@@ -4,38 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PickingNumbers // unsolved - 4/10
+namespace PickingNumbers
 {
     class PickingNumbers
     {
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            List<int> arr = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int counter = 0;
-            List<int> counters = new List<int>();
+            var subArr = new List<int>();
 
-            for (int i = 0; i < arr.Count; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < arr.Count; j++)
-                {
-                    if (i == j)
-                    {
-                        continue;
-                    }
+                int currentIndexCounter = 1;
 
-                    if (Math.Abs(arr[i] - arr[j]) == 0 || Math.Abs(arr[i] - arr[j]) == 1)
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (Math.Abs(arr[i] - arr[j]) <= 1)
                     {
-                        counter++;
+                        currentIndexCounter++;
                     }
                 }
 
-                counters.Add(counter);
-
-                counter = 0;
+                if (currentIndexCounter >= counter)
+                {
+                    counter = currentIndexCounter;
+                }
             }
 
-            Console.WriteLine(counters.Max());
+            Console.WriteLine(counter);
         }
     }
 }
