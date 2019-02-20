4d3d123f1c82fb28b248f35e6694c296987b9771
Nikola Siderov <iamnikkysiderov@gmail.com>
2019-02-20 10:24:59 +0300

    implemented paydesk class with properties and setup main

--- HackerRank-Exercises/Algorithms/ArrayLists/Java/BackendAssignment/src/CardClasses/GoldCard.java
+++ HackerRank-Exercises/Algorithms/ArrayLists/Java/BackendAssignment/src/CardClasses/GoldCard.java
@@ -20,19 +20,24 @@ public class GoldCard implements ICard {
     }
 
     public Double initialDiscountRate() {
-        if (CheckNumber(this.turnoverPreviousMonth)) {
-            Double result = Double.parseDouble(this.turnoverPreviousMonth.toString().substring(0, 1));
-
-            return result;
+        if (CheckNumberDigits(this.turnoverPreviousMonth)) {
+            if (DoubleCheckNumberForDigits(this.turnoverPreviousMonth)) {
+                Double result = Double.parseDouble(this.turnoverPreviousMonth.toString().substring(0, 1) + "0");
+                return result;
+            }
+            else {
+                Double result = Double.parseDouble(this.turnoverPreviousMonth.toString().substring(0, 1));
+                return result;
+            }
         }
         else {
             return 2.0;
         }
     }
 
-    private static Boolean CheckNumber(Double value) {
+    private static Boolean CheckNumberDigits(Double value) {
         if (value.toString().contains(".")) {
-            if (value.toString().split("\\.")[0].length() == 3) {
+            if (value.toString().split("\\.")[0].length() >= 3) {
                 return true;
             }
             else {
@@ -40,7 +45,26 @@ public class GoldCard implements ICard {
             }
         }
         else {
-            if (value.toString().length() == 3) {
+            if (value.toString().length() >= 3) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    private static Boolean DoubleCheckNumberForDigits(Double value) {
+        if (value.toString().contains(".")) {
+            if (value.toString().split("\\.")[0].length() >= 4) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            if (value.toString().length() >= 4) {
                 return true;
             }
             else {
--- HackerRank-Exercises/Algorithms/ArrayLists/Java/BackendAssignment/src/com/company/Main.java
+++ HackerRank-Exercises/Algorithms/ArrayLists/Java/BackendAssignment/src/com/company/Main.java
@@ -1,10 +1,69 @@
 package com.company;
 
-import CardClasses.GoldCard;
 import Contracts.ICard;
+import MarketStore.PayDesk;
+import CardClasses.BronzeCard;
+import CardClasses.SilverCard;
+import CardClasses.GoldCard;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        // input is specific:
+        // {cardType} {cardOwner} {turnover} {purchaseValue}
+        // each value separated by space
+        String[] input = getInput();
+
+        outputPayDeskValues(input);
+    }
+
+    public static String[] getInput() {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split("\\s");
+
+        scanner.close();
+
+        return input;
+    }
+
+    public static ICard determineCardType(String[] input) {
+        ICard card;
+        String cardType = input[0];
+        String cardOwner = input[1];
+        Double turnover = Double.parseDouble(input[2]);
+
+
+        switch (cardType) {
+            case "Bronze" : card = new BronzeCard(cardOwner ,turnover);
+                break;
+            case "Silver": card = new SilverCard(cardOwner ,turnover);
+                break;
+            case "Gold" : card = new GoldCard(cardOwner ,turnover);
+                break;
+            default: return null;
+        }
+
+        return card;
+    }
+
+    public static void outputPayDeskValues(String[] input) {
+        ICard card = determineCardType(input);
+
+        if (card != null) {
+            Double purchaseValue = Double.parseDouble(input[3]);
+
+            PayDesk payDesk = new PayDesk(purchaseValue, card);
+
+            System.out.printf("Purchase value: %.2f", payDesk.totalPurchaseValue());
+            System.out.println();
+            System.out.printf("Discount rate: %.2f", payDesk.discountRate());
+            System.out.println();
+            System.out.printf("Discount: %.2f", payDesk.discount());
+            System.out.println();
+            System.out.printf("Total: %.2f", payDesk.totalPrice());
+            System.out.println();
+        }
     }
 }
